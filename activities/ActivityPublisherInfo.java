300cb2b69e029df43b9b1330eb7c11b810b77027
Shahed Hasan <118453403+shcode998@users.noreply.github.com>
2023-04-16 11:20:59 -0400

    Add files via upload

---
+++ activities/ActivityPublisherInfo.java
@@ -0,0 +1,60 @@
+package com.app.androidnewsapp.activities;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.app.androidnewsapp.R;
+import com.app.androidnewsapp.database.prefs.SharedPref;
+import com.app.androidnewsapp.utils.Tools;
+
+public class ActivityPublisherInfo extends AppCompatActivity {
+
+    SharedPref sharedPref;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Tools.getTheme(this);
+        setContentView(R.layout.activity_privacy_policy);
+        Tools.setNavigation(this);
+        sharedPref = new SharedPref(this);
+        webView = findViewById(R.id.webView);
+        Tools.displayPostDescription(this, webView, sharedPref.getPublisherInfo());
+        setupToolbar();
+
+    }
+
+    public void setupToolbar() {
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (sharedPref.getIsDarkTheme()) {
+            toolbar.setBackgroundColor(getResources().getColor(R.color.colorToolbarDark));
+        } else {
+            toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        }
+
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setTitle(getResources().getString(R.string.title_about_publisher));
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+
+}
